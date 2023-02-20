4268d64cb245c1f5e261a30d03d361465ed1b5d3
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-20 15:42:39 +0530

    added homecontroller, tomcat jasper dependency

--- src/main/java/com/SpringBootJPA/demo/SpringBootJpaApplication.java
+++ src/main/java/com/SpringBootJPA/demo/SpringBootJpaApplication.java
@@ -1,4 +1,4 @@
-package com.SpringBootJPA.demo;
+ package com.SpringBootJPA.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/SpringBootJPA/demo/controller/AlienController.java
@@ -0,0 +1,14 @@
+package com.SpringBootJPA.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AlienController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home.jsp"; //for this to return jsp we need to add tomcat jasper.
+	}
+	
+}
