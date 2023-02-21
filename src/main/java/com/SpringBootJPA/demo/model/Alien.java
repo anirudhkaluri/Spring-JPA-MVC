de16d0a87e19b340e217ccd0aecec0fc25aae72b
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-21 09:53:59 +0530

    addaed jakarta persistence annotations to alien class

--- src/main/java/com/SpringBootJPA/demo/model/Alien.java
+++ src/main/java/com/SpringBootJPA/demo/model/Alien.java
@@ -1,7 +1,12 @@
 package com.SpringBootJPA.demo.model;
 
-public class Alien {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
+public class Alien {
+	
+	@Id
 	private int aid;
 	private String name;
 	public int getAid() {
