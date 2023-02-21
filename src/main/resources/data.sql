c05e853a8c6c8242db115476a40ab7552cd0a178
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-21 17:49:42 +0530

    changed data.sql file to include tech field in the queries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into ALIEN values (1,'anirudh');
-insert into ALIEN values (2,'avinash');
-insert into ALIEN values (3,'ravi');
-insert into ALIEN values (4,'pavan');
-insert into ALIEN values (5,'anish');
+insert into ALIEN values (1,'anirudh','java');
+insert into ALIEN values (2,'avinash','python');
+insert into ALIEN values (3,'ravi','android');
+insert into ALIEN values (4,'pavan','go');
+insert into ALIEN values (5,'anish','react');
