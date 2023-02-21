86944e745f57e6a8b984e0287701c96a6cb1b019
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-21 11:46:49 +0530

    added sql file to initialize in memory h2 everytime after relaunch

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into ALIEN values (1,'anirudh');
+insert into ALIEN values (2,'avinash');
+insert into ALIEN values (3,'ravi');
+insert into ALIEN values (4,'pavan');
+insert into ALIEN values (5,'anish');
