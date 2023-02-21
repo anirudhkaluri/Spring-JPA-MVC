9a28b1c9839ac817adcc587fd47d3a1bd25f0724
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-21 17:48:21 +0530

    changed the alien class to include additional field tech

--- src/main/java/com/SpringBootJPA/demo/model/Alien.java
+++ src/main/java/com/SpringBootJPA/demo/model/Alien.java
@@ -9,6 +9,13 @@ public class Alien {
 	@Id
 	private int aid;
 	private String name;
+	private String tech;
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
 	public int getAid() {
 		return aid;
 	}
@@ -23,7 +30,7 @@ public class Alien {
 	}
 	@Override
 	public String toString() {
-		return "Alien [aid=" + aid + ", name=" + name + "]";
+		return "Alien [aid=" + aid + ", name=" + name + ", tech=" + tech + "]";
 	}
 	
 }
