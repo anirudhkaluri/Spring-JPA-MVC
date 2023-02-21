57f7d18066c7bc1c6d7f604039289e60fc4a8fc4
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-21 20:33:05 +0530

    Custom complex query in AlienRepo findTechSorted

--- src/main/java/com/SpringBootJPA/demo/Dao/AlienRepo.java
+++ src/main/java/com/SpringBootJPA/demo/Dao/AlienRepo.java
@@ -2,6 +2,7 @@ package com.SpringBootJPA.demo.Dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.SpringBootJPA.demo.model.Alien;
@@ -9,5 +10,8 @@ import com.SpringBootJPA.demo.model.Alien;
 public interface AlienRepo extends CrudRepository<Alien,Integer>{
 	
 	List<Alien> findByTech(String tech);
+	List<Alien> findByAidGreaterThan(int aid);
+	@Query("from ALIEN where tech=?1 order by name")
+	List<Alien> findByTechSorted(String tech);
 
 }
