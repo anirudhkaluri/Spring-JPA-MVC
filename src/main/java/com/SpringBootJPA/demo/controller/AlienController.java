50108fff91473ce5891dfd4299a51ab80ec80640
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-21 14:00:22 +0530

    controller to recieve addAlien request

--- src/main/java/com/SpringBootJPA/demo/controller/AlienController.java
+++ src/main/java/com/SpringBootJPA/demo/controller/AlienController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.SpringBootJPA.demo.model.Alien;
+
 @Controller
 public class AlienController {
 	
 	@RequestMapping("/")
 	public String home() {
 		return "home.jsp"; //for this to return jsp we need to add tomcat jasper.
+	}
+	
+	@RequestMapping("/addAlien")
+	public String addAlien(Alien alien) {
+		
+		return "home.jsp";
 	}
 	
 }
