89c15bb56f31c58cec7cae83272f0360823e0832
anirudhkaluri <anirudhkaluri@gmail.com>
2023-02-23 16:43:08 +0530

    deleteAlien api added

--- src/main/java/com/SpringBootJPA/demo/controller/AlienController.java
+++ src/main/java/com/SpringBootJPA/demo/controller/AlienController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -130,10 +132,19 @@ public class AlienController {
 	}
 	*/
 	
-	@PostMapping("/alien")
-	public String addAlien(Alien alien) {
-		repo.save(alien);
-		return "home.jsp";
+	@PostMapping(path="/alien",consumes= {"application/json"}) //no need of @ResponseBody 
+	public Alien addAlien(@RequestBody Alien alien) {
+		repo2.save(alien);
+		return alien;
+	}
+	
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable("aid") int aid) {
+		//first verify if there is aid
+		Alien al= repo2.getOne(aid);
+		repo.delete(al);
+		
+		return "deleted";
 	}
 	
 	
